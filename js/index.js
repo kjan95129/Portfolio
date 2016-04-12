845aa11e6d1e6d8b1d72c8700a5b089c5ccb43ca
Kevin Jan <kjan95129@gmail.com>
2016-04-12 16:28:26 -0700

    fade in title

--- js/index.js
+++ js/index.js
@@ -59,9 +59,7 @@ $(window).scroll(function() {
     $(".progress-bar").each(function(){
         each_bar_width = $(this).attr('aria-valuenow');
         $(this).width(each_bar_width + '%');
-    });
-
-       
+    });  
  }  
 });
 
