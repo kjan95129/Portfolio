bdfdb637d6c28a237469c3465e2a9121e54d2d46
Kevin Jan <kjan95129@gmail.com>
2016-06-21 21:19:49 -0700

    Poop picture

--- js/index.js
+++ js/index.js
@@ -64,6 +64,14 @@ $(window).scroll(function() {
  }  
 });
 
+/*$('window').on('touchmove', function(event) {
+    jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},1500);
+	});
+}*/
+
 /*jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
