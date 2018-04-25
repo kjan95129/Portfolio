f8f51588f8955c517def50302530c03b4baa8b73
Kevin <kjan95129@gmail.com>
2018-04-25 03:47:39 -0700

    ??

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 $('a').click(function(e) {
     e.preventDefault();
     newLocation = this.href;
-    $('body').fadeOut(newpage);
+    $('body').fadeOut('slow', newpage);
 });
 
 function newpage() {
