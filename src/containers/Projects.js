f12415a17268caa07f12cd432fa71dd500789107
Kevin Jan <kejan@cisco.com>
2019-04-25 01:27:44 -0700

    Added two projects

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -5,15 +5,23 @@ import clickndigLogo from 'static/cards/clickndig.png'
 import brewngoLogo from 'static/cards/brewngo.png'
 import violetLogo from 'static/cards/violet.png'
 import teamwatchLogo from 'static/cards/teamwatch.png'
+import buildacatLogo from 'static/cards/buildacat.png'
+import wwgfbtLogo from 'static/cards/wwgfbt.png'
 
 const projects = [
   {
-    link: 'grape',
-    externalLink: 'http://grapeapp.herokuapp.com/',
-    image: grapeLogo,
-    header: 'do something nice today.',
-    description: 'Mobile web application designed to encourage users to track personal acts of kindness',
-    caseStudy: true,
+    link: 'wwgfbt',
+    externalLink: 'https://wwgfbt.com/',
+    image: wwgfbtLogo,
+    header: 'did someone say boba???',
+    description: 'Web application to help you choose where to go for bubble tea when you\'re feeling indecisive'
+  },
+  {
+    link: 'teamwatch',
+    externalLink: 'https://soccer-app-a33d8.firebaseapp.com/index.html',
+    image: teamwatchLogo,
+    header: 'keep track of your team',
+    description: 'Progressive Web Application that allows users to manage small-scale soccer teams & games',
   },
   {
     link: 'violet',
@@ -24,19 +32,27 @@ const projects = [
     caseStudy: true,
   },
   {
+    link: 'buildacat',
+    externalLink: 'http://build-a-cat.herokuapp.com',
+    image: buildacatLogo,
+    header: 'find the cutest cat. ever.',
+    description: 'Web application that shows you pictures of cats that match the facial features you specify'
+  },
+  {
+    link: 'grape',
+    externalLink: 'http://grapeapp.herokuapp.com/',
+    image: grapeLogo,
+    header: 'do something nice today.',
+    description: 'Mobile web application designed to encourage users to track personal acts of kindness',
+    caseStudy: true,
+  },
+  {
     link: 'brewngo',
     externalLink: 'http://bruvida.com/',
     image: brewngoLogo,
     header: 'coffee & tea. on the go.',
     description: 'Innovative product for an automatic and portable coffee & tea brewer',
     caseStudy: true,
-  },
-  {
-    link: 'teamwatch',
-    externalLink: 'https://soccer-app-a33d8.firebaseapp.com/index.html',
-    image: teamwatchLogo,
-    header: 'keep track of your team',
-    description: 'Progressive Web Application that allows users to manage small-scale soccer teams & games',
   },
   {
     link: 'clickndig',
