b60e47ad85c970c04bd93819afb33133783b0aad
Kevin Jan <kjan95129@gmail.com>
2019-10-16 00:13:25 -0700

    poke

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from 'containers/App'
+import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 
 import 'font-awesome/css/font-awesome.min.css'
